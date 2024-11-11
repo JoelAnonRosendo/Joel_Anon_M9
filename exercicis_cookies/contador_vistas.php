f249cbdbb072c919112a9021c1590e26c464893a
Joel Añón <117432577+JoelAnonRosendo@users.noreply.github.com>
2024-11-11 19:21:46 +0100

    Update contador_vistas.php

--- exercicis_cookies/contador_vistas.php
+++ exercicis_cookies/contador_vistas.php
@@ -1,12 +1,4 @@
 <?php
-// El código se basa en un contador de visitas que utiliza una cookie (visitas)
-// que se incrementa cada vez que el usuario accede a la página de la tienda.
-// En caso de que el usuario realice 5 visitas, se le muestra un descuento del 20%,
-// y si llega a 10 visitas, se le ofrece un 50% de descuento.
-// Pero, si el usuario ya ha realizado una compra con descuento, esta quedará registrada
-// en otra cookie (compra_realizada) y no se le ofrecerá ningún descuento adicional.
-// Para que se puedan aplicar otra vez los descuentos, las cookies deben ser borradas,
-// así otro usuario podrá tener su descuento y de esta manera con el resto de usuarios.
 	if (isset($_COOKIE['visitas'])) {
     		$visitas = $_COOKIE['visitas'] + 1;
 	} else {
