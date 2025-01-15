363ba00ac727c7e6434fe844b8861c0c9c726919
JoelAnonRosendo <janonrosendo@gmail.com>
2025-01-15 21:21:47 +0100

    ejercicio dia 13/01

---
+++ demo1301/notes.php
@@ -0,0 +1,19 @@
+<?php
+
+
+$nota = $_GET['nota'];
+
+$conn = mysqli_connect("localhost", "janon", "joel", "test");
+
+if(!$conn){
+    echo "No se ha podido conectar a la BBDD";
+}
+else {
+    #$sql = "SELECT * FROM usuaris WHERE nota > $nota";
+    #$query = mysqli_query($conn, $sql);
+    #$rows = mysqli_num_rows($query);
+    #echo "Hay $rows alumnos con mas de un $nota .";
+    $query = mysqli_query($conn, "INSERT INTO usuaris VALUES ('48215685A', 'Joel', 'Anon', '7')");
+    $consulta = mysqli_affected_rows($conn);
+}
+?>
