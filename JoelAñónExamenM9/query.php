84265160e186a8001a0ec73e687e67b4a7f2074f
Joel Añón <117432577+JoelAnonRosendo@users.noreply.github.com>
2025-03-05 21:15:51 +0100

    Update query.php
    .

--- JoelAñónExamenM9/query.php
+++ JoelAñónExamenM9/query.php
@@ -2,7 +2,7 @@
 $conn=mysqli_connect("localhost", "joelAnonExam", "Admin1234", "joelAnonExam");
 $puntuacion=$_POST["puntuacion"];
 echo "Selección usuario: $puntuacion";
-$sql="SELECT TITULO,ANYO FROM `peliculas` WHERE PUNTUACION < $puntuacion;";
+$sql="SELECT TITULO,ANYO FROM `peliculas` WHERE PUNTUACION > $puntuacion;";
 $query=mysqli_query($conn,$sql);
 while($row = mysqli_fetch_array($query)){
     echo $row['TITULO'];
